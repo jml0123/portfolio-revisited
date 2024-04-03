fb0be08ae99bdf22efb2f2d3eefdf3bdd35ec13e
miguel l <m_n_l@miguels-mbp.mynetworksettings.com>
2024-04-03 02:37:46 -0400

    more writeup styles

--- pages/write-ups/audio-hijack/index.tsx
+++ pages/write-ups/audio-hijack/index.tsx
@@ -1,12 +1,15 @@
 import writeUp from "../write-up"
-
+import styles from '../../../styles/scss/WriteUp.module.scss'; 
 
 function AudioHijack() {
   return(
     <div>
         <h1>Audio Hijack</h1>
-        <p>Lorem Ipsum</p>
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem Ipsum dolor...</p>
+        <div className={styles['write-up-img']}>
         <img src='/img/artwork/bk_night.webp'></img>
+        </div>
     </div>
   )
 };
